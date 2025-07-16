4a3e68f79ca76b9d5b3d5adb5dbc8e4011e6dbfb
Matthew Richards <matthewrichards234@gmail.com>
2025-07-16 11:35:34 -0400

    Create initialCards arr

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,37 @@
+// Looping Through an Array of Objects
+
+/* To - Do :
+1. Get 6 images.
+2. Compress them.
+3. Add them to images folder.
+4. Link them via relative path */
+const initialCards = [
+  {
+    name: "Object 1",
+    link: "../images\tsuyoshi-kozu-9luf51j0R-0-unsplash-min  (1).jpg",
+  },
+  {
+    name: "Object 2",
+    link: "../images\tsuyoshi-kozu-9luf51j0R-0-unsplash-min  (2).jpg",
+  },
+  {
+    name: "Object 3",
+    link: "../images\tsuyoshi-kozu-9luf51j0R-0-unsplash-min  (3).jpg",
+  },
+  {
+    name: "Object 4",
+    link: "../images\tsuyoshi-kozu-9luf51j0R-0-unsplash-min  (4).jpg",
+  },
+  {
+    name: "Object 5",
+    link: "../images\tsuyoshi-kozu-9luf51j0R-0-unsplash-min  (5).jpg",
+  },
+  {
+    name: "Object 6",
+    link: "../images\tsuyoshi-kozu-9luf51j0R-0-unsplash-min  (6).jpg",
+  },
+];
+
 // Edit Profile & Add Post buttons selected.
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const addPostBtn = document.querySelector(".profile__add-btn");
@@ -28,3 +62,7 @@ exitProfileModal.addEventListener("click", () =>
 exitPostModal.addEventListener("click", () =>
   addPostModal.classList.remove("modal_is-opened")
 );
+
+initialCards.forEach(function (item) {
+  console.log(item.name);
+});
