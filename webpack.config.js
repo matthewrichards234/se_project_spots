6406f89a2eb852c33dd12fec73d579fba150964b
Matthew Richards <matthewrichards234@gmail.com>
2025-08-07 10:03:56 -0400

    Restyle IMG tags for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: "./src/index.html",
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
