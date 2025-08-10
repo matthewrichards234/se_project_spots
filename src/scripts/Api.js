3889321a4a80221358aa4ee416e389843f1b3311
Matthew Richards <matthewrichards234@gmail.com>
2025-08-10 10:27:36 -0400

    create Api class, import api key

---
+++ src/scripts/Api.js
@@ -0,0 +1,15 @@
+class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _handleServerResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error: ${res.status}`);
+  }
+}
+
+export default Api;
